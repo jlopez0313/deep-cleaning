788be809b08674daac792ef1927cdb15ebf40b37
Jonathan Lopez <jlopez0313@hotmail.com>
2023-05-10 10:34:14 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'deep-mobile',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
